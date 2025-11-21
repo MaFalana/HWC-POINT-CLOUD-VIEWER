e28dbee8a1a686071a7d877c8d365654576048a2
MaFalana <malikfalana@icloud.com>
2025-11-21 06:48:43 -0500

    Add project card component structure and styling setup

--- src/components/Dashboard/Card/main.jsx
+++ src/components/Dashboard/Card/main.jsx
@@ -1,8 +1,14 @@
+import '../../../styles/card.css'
+
 export function HWCCard({ project }) {
     return (
         <div className="hwc-card">
-            <h3>{project.name || project.id}</h3>
-            {/* Add your card content here */}
+            <img src={project.thumbnail} alt={`${project.name} thumbnail image`} />
+            <h1>{project.name}</h1>
+            <h2>{project.id}</h2>
+            <h3>{project.date}</h3>
+            <p>{project.client}</p> {/* Add the tags parameter after client later will be a list that needs to be mapped*/}
+            <button>View Project</button> {/* Use a vertical ellipsis icon */}
         </div>
     );
 }
--- src/components/Dashboard/main.jsx
+++ src/components/Dashboard/main.jsx
@@ -1,2 +1,4 @@
+
 import { HWCCard } from "./Card/main";
 import { HWCMap } from "./Map/main";
+
